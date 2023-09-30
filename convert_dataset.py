20b55ae50d8a8558f835c864b3d456e4e2c2ffcd
KonstFed <konstantin_fedorov_03@mail.ru>
2023-09-30 20:14:46 +0300

    parralelism added

--- convert_dataset.py
+++ convert_dataset.py
@@ -1,10 +1,11 @@
 import os
+from multiprocessing import Pool
 
 from PIL import Image
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from tqdm import trange
+from tqdm import tqdm
 
 from dataset import get_full_dataset
 from face_crop import crop_resize
@@ -14,24 +15,30 @@ def _load(path: str):
     # return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
 
-def save(img: np.ndarray, path: str):
+def _save(img: np.ndarray, path: str):
     p_img = Image.fromarray(img)
     p_img.save(path)
-    
+
+OUT_PATH = "datasets/test_faces_dataset"
+
+def convert_single(obj):
+    path, label = obj
+    img = _load(path)
+    filename = path.split("/")[-1]
+    face_image = crop_resize(img, resize_to=300)
+    if label == 1:
+        _save(face_image, OUT_PATH + "/living/" + filename)
+    else:
+        _save(face_image, OUT_PATH + "/fake/" + filename)
 
 def convert(path: str, out: str):
     dataset = get_full_dataset(path, resize=None)
     # os.makedirs(out + "/living")
     # os.makedirs(out + "/fake")
+    pool = Pool(5)
+    for _ in tqdm(pool.imap_unordered(convert_single, dataset.data), total=len(dataset)):
+        pass
 
-    for i in trange(len(dataset)):
-        image = _load(dataset.data[i][0])
-        filename = dataset.data[i][0].split("/")[-1]
-        face_image = crop_resize(image, resize_to=300)
-        if dataset.data[i][1] == 1:
-            save(face_image, out + "/living/" + filename)
-        else:
-            save(face_image, out + "/fake/" + filename)
 
 if __name__ == "__main__":
     convert("datasets/test_dataset", "datasets/test_faces_dataset")
